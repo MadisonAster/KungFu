d9187bdea13819be1f974bc2efb6b46b45df890a
Madison Aster <info@MadisonAster.com>
2020-08-05 22:04:50 -0700

    yay walrus operator!

--- Go/HelloWorld.go
+++ Go/HelloWorld.go
@@ -4,4 +4,22 @@ import "fmt"
 
 func main() {
     fmt.Println("hello world")
+    fmt.Println("go" + "lang")
+
+    fmt.Println("1+1 =", 1+1)
+    fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+    //var a = "initial"
+    var b int = 100
+    fmt.Println(b)
+
+
+    var twoD [2][3]int
+    for i := 0; i < 2; i++ {
+    	for j := 0; j < 3; j++ {
+    		twoD[i][j] = i + j
+    	}
+    	break
+    }
+    fmt.Println(twoD)
 }
