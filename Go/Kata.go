dd5f46396bbf13bd2f915e77108889370143047e
Madison Aster <info@MadisonAster.com>
2020-08-06 23:37:25 -0700

    apparently my test names were incorrect, must follow Test*

--- Go/Kata.go
+++ Go/Kata.go
@@ -18,10 +18,10 @@ func main() {
 
     var twoD [2][3]int
     for i := 0; i < 2; i++ {
-    	for j := 0; j < 3; j++ {
-    		twoD[i][j] = i + j
-    	}
-    	break
+        for j := 0; j < 3; j++ {
+            twoD[i][j] = i + j
+        }
+        break
     }
     fmt.Println(twoD)
 }
@@ -38,3 +38,26 @@ func RepeatStr(repetitions int, value string) string {
 func IsDivisible(n, x, y int) bool {
     return (n%x==0 && n%y==0)
 }
+
+
+
+func listrange(start, end int) []int {
+    //for N := start; N < end; N++ {
+    s := make([]int, end-start)
+    for i := range s {
+        s[i] = i
+    }
+    return s
+    //}
+}
+
+
+/*
+func BecomeImmortal(m int, n int, l int, t int) int {
+    rowvalue = m-1
+    rowsubtract = l*(m-1)
+    row = rowvalue - rowsubtract
+    total = row * n
+    return total % 100
+}
+*/
--- Go/Kata_test.go
+++ Go/Kata_test.go
@@ -2,30 +2,58 @@ package Kata
 
 import "testing"
 import "strconv"
+import "reflect"
+//import "strings"
+import "fmt"
 
-func GetMD5Hash_test1(t *testing.T) {
-	input := "password"
-	expected := "5f4dcc3b5aa765d61d8327deb882cf99"
-	actual := GetMD5Hash(input)
-	if actual != expected {
-		t.Errorf("Test failed! Expected: '%s', got: '%s'", expected, actual)
-	}
+//func formatlist(A []int) {
+//    return strings.Trim(strings.Replace(fmt.Sprint(A), " ", ",", -1), "[]")
+//}
+
+func TestGetMD5Hash_1(t *testing.T) {
+    input := "password"
+    expected := "5f4dcc3b5aa765d61d8327deb882cf99"
+    actual := GetMD5Hash(input)
+    if actual != expected {
+        t.Errorf("Test failed! Expected: '%s', got: '%s'", expected, actual)
+    }
 }
 
-func RepeatStr_test1(t *testing.T) {
-	expected := "aaaa"
-	actual := RepeatStr(4, "a") 
-	if actual != expected {
-		t.Errorf("Test failed! Expected: '%s', got: '%s'", expected, actual)
-	}
+func TestRepeatStr_1(t *testing.T) {
+    expected := "aaaa"
+    actual := RepeatStr(4, "a") 
+    if actual != expected {
+        t.Errorf("Test failed! Expected: '%s', got: '%s'", expected, actual)
+    }
+}
+func TestRepeatStr_2(t *testing.T) {
+    expected := "abababab"
+    actual := RepeatStr(4, "ab") 
+    if actual != expected {
+        t.Errorf("Test failed! Expected: '%s', got: '%s'", expected, actual)
+    }
+}
+
+func TestIsDivisible_1(t *testing.T) {
+    expected := false
+    actual := IsDivisible(3, 3, 4) 
+    if actual != expected {
+        t.Errorf("Test failed! Expected: '%s', got: '%s'", strconv.FormatBool(expected), strconv.FormatBool(actual))
+    }
 }
 
 
-func IsDivisible_test1(t *testing.T) {
-	expected := false
-	actual := IsDivisible(3, 3, 4) 
-	if actual != expected {
-		t.Errorf("Test failed! Expected: '%s', got: '%s'", strconv.FormatBool(expected), strconv.FormatBool(actual))
-	}
+func Test_listrange_1(t *testing.T) {
+    expected := []int {0,1,2,3}
+    //actual := []int {1,2,3,4}
+    actual := listrange(0,4)
+    fmt.Println(reflect.DeepEqual(actual, expected))
+
+    if !reflect.DeepEqual(actual, expected) {
+    //if true {
+        t.Errorf("list range test failed!")
+    //    t.Errorf("Test failed! Expected: '%s', got: '%s'", formatlist(expected), formatlist(actual))
+    }
 }
+
 
