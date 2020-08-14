5a3d1b9f9b4d95938285cfcb0fde0234f62671e3
Madison Aster <info@MadisonAster.com>
2020-08-14 15:12:43 -0700

    prerequisites, and resizing

--- Python/Qt/BasicWindow.py
+++ Python/Qt/BasicWindow.py
@@ -11,11 +11,28 @@ TestKit.SingletonApp() #Global because it QApplication must be a singleton
 class BasicWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(BasicWindow, self).__init__(*args, **kwargs)
+        self.setWindowTitle('Basic Window')
+        #self.setDockOptions(False)
+        #self.setDockNestingEnabled(True)
+        self.setWindowTitle('Basic Window')
+
+        self.QAvailableGeo = QtWidgets.QDesktopWidget().availableGeometry()
+        self.QStartBarHeight = QtWidgets.QDesktopWidget().screenGeometry(0).height()-self.QAvailableGeo.height()
+        
+    def sizeHint(self):
+        return QtCore.QSize(800,600)
 
 class test_BasicWindow(TestKit.TimedTest):
     def test_1(self):
         self.MainWindow = BasicWindow()
         self.MainWindow.show()
+        time.sleep(0.5)
+
+    def test_2(self):
+        self.MainWindow = BasicWindow()
+        self.MainWindow.show()
+        self.MainWindow.resize(self.MainWindow.QAvailableGeo.width()/2, self.MainWindow.QAvailableGeo.height()-self.MainWindow.QStartBarHeight)
+        self.MainWindow.move(0,0)
         time.sleep(0.5)
 
 if __name__ == '__main__':
