3a0a6aa7a675e6cb60bb67c1d965b4cc146f6dc1
Madison Aster <info@MadisonAster.com>
2020-08-15 17:23:13 -0700

    formatting

--- C++/Algorithms/Math.cpp
+++ C++/Algorithms/Math.cpp
@@ -1,5 +1,5 @@
 //
-//
+//General Math
 //
 
 int max(int a, int b) {
@@ -10,19 +10,17 @@ int max(int a, int b) {
 		result = b;
 	return result;
 }
-
-bool isDivisible(int n, int x, int y) {
-	return n % x == 0 && n % y == 0;
-}
-
 TEST(pch, test_max) {
 	EXPECT_EQ(5, max(5, 4));
 }
 
+
+bool isDivisible(int n, int x, int y) {
+	return n % x == 0 && n % y == 0;
+}
 TEST(pch, test_isDivisible1) {
 	EXPECT_EQ(false, isDivisible(3, 3, 4));
 }
-
 TEST(pch, test_isDivisible2) {
 	EXPECT_EQ(true, isDivisible(12, 3, 4));
 }
--- C++/Algorithms/Math.h
+++ C++/Algorithms/Math.h
@@ -5,3 +5,4 @@
 #pragma once
 
 int max(int a, int b);
+bool isDivisible(int n, int x, int y);
