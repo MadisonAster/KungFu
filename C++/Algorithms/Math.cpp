ec9739e99e7fc8afa3a3f749d56bce9130c66494
Madison Aster <info@MadisonAster.com>
2020-08-15 17:21:23 -0700

    isDivisible

--- C++/Algorithms/Math.cpp
+++ C++/Algorithms/Math.cpp
@@ -11,6 +11,18 @@ int max(int a, int b) {
 	return result;
 }
 
+bool isDivisible(int n, int x, int y) {
+	return n % x == 0 && n % y == 0;
+}
+
 TEST(pch, test_max) {
 	EXPECT_EQ(5, max(5, 4));
+}
+
+TEST(pch, test_isDivisible1) {
+	EXPECT_EQ(false, isDivisible(3, 3, 4));
+}
+
+TEST(pch, test_isDivisible2) {
+	EXPECT_EQ(true, isDivisible(12, 3, 4));
 }
