7bc8664789aa07887599dadb42ef8b2a45d5082a
Madison Aster <info@MadisonAster.com>
2020-08-27 13:23:38 -0700

    moving stuff around

---
+++ Python/Algorithms/Searching/JumpSearch.py
@@ -0,0 +1,35 @@
+import unittest
+import random
+from datetime import datetime
+
+def JumpSearch(Array, n, low=0, high=None):
+    if high == None:
+        high = len(Array)-1
+    if high >= low:
+        mid = low + (high - low) // 2
+        if Array[mid] == n:
+            return mid
+        elif Array[mid] > n:
+            return JumpSearch(Array, n, low=low, high=mid-1)
+        else:
+            return JumpSearch(Array, n, low=mid+1, high=high)
+    else:
+        return -1
+
+class test_JumpSearch(unittest.TestCase):
+    def setUp(self):
+        self.starttime = datetime.now()
+    def tearDown(self):
+        t = datetime.now() - self.starttime
+        print(str(t), self.id())
+
+    def test_1(self):
+        Array = list(range(100000))
+        self.assertEqual(JumpSearch(Array, Array[-1]), len(Array)-1)
+
+    def test_std(self):
+        Array = list(range(100000))
+        self.assertEqual(Array.index(Array[-1]), len(Array)-1)
+
+if __name__ == '__main__':
+    unittest.main()
