47d8b64a86f5616ca112aaa1a0517d8b76cbf216
Madison Aster <info@MadisonAster.com>
2020-08-27 16:28:42 -0700

    Test3

--- Python/Algorithms/_CodeWars/RATest.py
+++ Python/Algorithms/_CodeWars/RATest.py
@@ -61,7 +61,6 @@ def words(filepath):
             if buffer != '':
                 yield buffer
             buffer = ''
-
 class test_words(unittest.TestCase):
     def setUp(self):
         self.starttime = datetime.now()
@@ -77,6 +76,41 @@ class test_words(unittest.TestCase):
         with open(self.mock_path, 'w') as file:
             file.write(mocktext)
         self.assertEqual(list(words(self.mock_path)), ['abcde']*5)
+
+
+
+def find(nums, target):
+    ibuffer = []
+    def recurse_find(nums, ibuffer=None):
+        for i, a in enumerate(nums):
+            ibuffer.append(i)
+            if type(a) == list:
+                for b in recurse_find(a, ibuffer=ibuffer):
+                    yield b
+            else:
+                yield a
+            ibuffer.pop(-1)
+    for val in recurse_find(nums, ibuffer=ibuffer):
+        if val == target:
+            return ibuffer
+
+
+class test_find(unittest.TestCase):
+    def setUp(self):
+        self.starttime = datetime.now()
+    def tearDown(self):
+        t = datetime.now() - self.starttime
+        print(str(t), self.id())
+
+    def test_1(self):
+        nums = [0, 42, [300, [], 200, [[101]]], 1000, 200]
+        expected = [2, 3, 0, 0]
+        self.assertEqual(find(nums, 101), expected)
+
+        target = nums
+        for i in expected:
+            target = target[i]
+        self.assertEqual(target, 101)
 
 
 
