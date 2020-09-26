2ecdf1d6bcf5c3ae6be81e8ea5f3a686671ec63d
Madison Aster <info@MadisonAster.com>
2020-09-25 17:35:18 -0700

    ipywidgets install

---
+++ _installers/ipywidgets_install.sh
@@ -0,0 +1,5 @@
+#pip install matplotlib
+#pip install scipy
+pip install ipywidgets
+jupyter nbextension enable --py widgetsnbextension --sys-prefix
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
