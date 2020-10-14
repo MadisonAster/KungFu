71bfa591a36ac3ebdf7686cae1a27232f6d0f9cc
Madison Aster <info@MadisonAster.com>
2020-10-14 16:25:34 -0700

    evolving

--- AWS/EKS/EFSCluster/EFSCluster.tf
+++ AWS/EKS/EFSCluster/EFSCluster.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 0.12.0"
 }
+provider "aws" {
+    version = ">= 2.28.1"
+    region  = var.region
+}
 
 module "EKSCluster" {
   source = "../EKSCluster"
@@ -20,12 +24,12 @@ resource "aws_efs_file_system" "KungFu_FileSystem" {
 }
 
 resource "aws_efs_mount_target" "KungFu_MountTargets" {
-  count = length(module.vpc.public_subnets)
+  count = length(module.EKSCluster.public_subnets)
 
   file_system_id  = aws_efs_file_system.KungFu_FileSystem.id
-  subnet_id = element(module.vpc.public_subnets, count.index)
+  subnet_id = element(module.EKSCluster.public_subnets, count.index)
   security_groups = [
-    module.vpc.default_security_group_id,
+    module.EKSCluster.default_security_group_id,
     aws_security_group.KungFu_ControlPlaneSecurityGroup.id,
     aws_security_group.KungFu_WebserverSecurityGroup.id,
     aws_security_group.KungFu_DataScraperSecurityGroup.id,
