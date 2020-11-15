49e16b9878ed4c8bf0274cfa988e31eb8bb2f102
Madison Aster <info@MadisonAster.com>
2020-11-14 23:42:39 -0800

    updating test project

--- Unreal/Source/KungFu.Target.cs
+++ Unreal/Source/KungFu.Target.cs
@@ -5,10 +5,10 @@ using System.Collections.Generic;
 
 public class KungFuTarget : TargetRules
 {
-	public KungFuTarget( TargetInfo Target) : base(Target)
+	public KungFuTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange( new string[] { "KungFu" } );
+		ExtraModuleNames.Add("KungFu");
 	}
 }
